27e14c4149fb7dde0bf80653849a80b76077a161
lucafrance <62500301+lucafrance@users.noreply.github.com>
2025-12-29 10:32:20 +0100

    Move package to src folder

--- scripts/__main__.py
+++ scripts/__main__.py
@@ -3,7 +3,7 @@ import logging
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "src"))
 
 from vipera.parser import VbaDocs
 
