02a8651cb69a2d5e0ac51bab99779a2af0c0ccb4
lucafrance <62500301+lucafrance@users.noreply.github.com>
2025-12-29 17:14:30 +0100

    Add Office Module test

---
+++ tests/test_office.py
@@ -0,0 +1,12 @@
+import unittest
+
+from vipera.office import excel
+
+class TestApplicationOpenClose(unittest.TestCase):
+    
+    def test_excel(self):
+        xl_app = excel.Application().new()
+        self.assertIs(type(xl_app), excel.Application)
+        xl_app.Visible = True
+        self.assertTrue(xl_app.Visible)
+        xl_app.Quit()
