472232d80c51b205b3c9b1c59e11f827acee3fbe
lucafrance <62500301+lucafrance@users.noreply.github.com>
2025-12-29 15:20:47 +0100

    Skip submodules initialisation if redundant

--- build_package.ps1
+++ build_package.ps1
@@ -1,5 +1,9 @@
-git submodule update --init
+if (-not (Test-Path "office-vba-reference\.git")) {
+    git submodule update --init
+}
+
 python -m scripts
 python -m pip install -e .
 # TODO run tests
 python -m build
+
